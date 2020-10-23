e256691b438a9d1ef4f7e9a080aea1c313e4f8e6
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-23 12:34:54 +0200

    more zad 3

--- zad-3/SQLQuery1.sql
+++ zad-3/SQLQuery1.sql
@@ -81,3 +81,93 @@ select tytul as tytulK, cena as cenaK, [cena jest]=CASE
 	else 'Nieznana'
 	end
 from biblioteka..ksiazki
+GO
+
+-- NULLIF
+-- przydatne, kiedy trzeba pomin¹æ jak¹œ wartoœæ w funkcjach agreguj¹cych
+-- proszê stworzyæ w³asny
+-- przyk³ad
+
+/*
+select
+count(*) as [Liczba pracowników],
+avg(nullif(zarobki, 0)) as [Œrednia p³aca],
+min(nullif(zarobki, 0)) as [P³aca minimalna]
+from test..pracownicy
+*/
+
+SELECT 
+	avg(nullif(year(getdate()) - year(t.data_ur_T), 0)) AS [sredni wiek trenera],
+	avg(nullif(year(getdate()) - year(z.data_ur), 0)) AS [sredni wiek zawodników]
+FROM narciarze.dbo.trenerzy t, narciarze.dbo.zawodnicy z
+GO
+
+-- ISNULL
+-- pozwala na nadawanie wartoœci domyœlnych tam, gdzie jest NULL
+-- proszê stworzyæ w³asny przyk³ad
+
+/*
+select title, pub_id, isnull(price, (select MIN(price) from pubs..titles)) from pubs..titles
+*/
+
+select isnull(t.imie_t, 'Zepsuty Trener') imie, t.nazwisko_t, isnull(t.data_ur_t, '02-04-2005') data_ur_t  from narciarze.dbo.trenerzy t
+GO
+
+-- Komunikaty o b³êdzie --
+RAISERROR (21000,10,1)
+PRINT @@error
+GO
+
+RAISERROR (21000, 10,1) WITH SETERROR
+PRINT @@error
+GO
+
+RAISERROR (21000, 11,1)
+PRINT @@error
+GO
+
+RAISERROR ('Ala ma kota',11,1)
+PRINT @@error
+GO
+
+DECLARE @d1 DATETIME, @d2 DATETIME
+SET @d1 = '20091020'
+SET @d2 = '20091025'
+
+SELECT dateadd(HOUR, 112, @d1)
+SELECT dateadd(DAY, 112, @d1)
+
+SELECT datediff(MINUTE, @d1, @d2)
+SELECT datediff(DAY, @d1, @d2)
+
+SELECT datename(MONTH, @d1)
+SELECT datepart(MONTH, @d1)
+
+SELECT CAST(day(@d1) AS VARCHAR)
+           + '-' + cast(month(@d1) AS VARCHAR)
+           + '-' + cast(year(@d1) AS VARCHAR)
+GO
+
+select COL_LENGTH('biblioteka..pracownicy', 'imie')
+
+select DATALENGTH(2+3.4)
+
+select db_id('master')
+
+select db_name(1)
+
+select user_id()
+
+select user_name()
+
+select host_id()
+
+select HOST_NAME()
+
+use biblioteka;
+
+select object_id('Pracownicy')
+
+select object_name(object_id('Pracownicy'))
+
+GO
