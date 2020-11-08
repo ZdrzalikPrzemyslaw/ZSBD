7b2e40cb12ff35acc6b0431e9e95714c8d705039
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-08 17:46:40 +0100

    zad 4 fin

--- zad-4/zad4.sql
+++ zad-4/zad4.sql
@@ -62,15 +62,19 @@ go
 create database testowa
 go
 create table testowa..oddzialy (
-	NR_ODD int primary key identity(1,1),
+	NR_ODD int primary key,
 	NAZWA_ODD varchar(30))
 go
 
-insert into testowa..oddzialy (NAZWA_ODD) values ('oddzial pierwszy')
+insert into testowa..oddzialy (NR_ODD, NAZWA_ODD) values (1, 'oddzial pierwszy')
 go
-insert into testowa..oddzialy (NAZWA_ODD) values ('oddzial drugi')
+insert into testowa..oddzialy (NR_ODD, NAZWA_ODD) values (2, 'oddzial drugi')
 go
-insert into testowa..oddzialy (NAZWA_ODD) values ('oddzial trzeci')
+insert into testowa..oddzialy (NR_ODD, NAZWA_ODD) values (3, 'oddzial trzeci')
+go
+insert into testowa..oddzialy (NR_ODD, NAZWA_ODD) values (4, 'oddzial czwarty')
+go
+insert into testowa..oddzialy (NR_ODD, NAZWA_ODD) values (5, 'oddzial piaty')
 go
 
 /* 7 */
@@ -81,5 +85,64 @@ begin
 	declare @name varchar(30) = (select NAZWA_ODD from testowa..oddzialy where @id = NR_ODD)
 	print 'nazwa oddzialu to ' + @name
 end
+go
 
 /* 8 */
+
+declare kursor cursor for (select * from testowa..oddzialy)
+declare @id int, @name varchar(30)
+open kursor
+fetch next from kursor into @id, @name
+while @@FETCH_STATUS = 0
+	begin
+		print 'NUMER ODDZIALU TO: ' + cast(@id as varchar) + ', NAZWA ODDZIALU TO: ' + @name
+		fetch next from kursor into @id, @name
+	end
+close kursor
+deallocate kursor
+go
+
+/* 9 */
+
+declare kursor cursor for (select nr_odd from testowa..oddzialy where NR_ODD > 2) for update
+declare @count int = 0
+declare @id int
+open kursor
+fetch next from kursor into @id
+while @@FETCH_STATUS = 0
+	begin
+		delete from testowa..oddzialy where current of kursor
+		set @count = @count + 1
+		fetch next from kursor into @id
+	end
+if @count > 0
+	print 'LICZBA USUNIETYCH REKORDOW: ' + cast(@count as varchar)
+close kursor
+deallocate kursor
+go
+
+
+/* 10 */
+
+declare kursor cursor for (select nr_odd from testowa..oddzialy where NR_ODD = 3) for update
+declare @id int
+open kursor
+fetch next from kursor into @id
+begin
+if @@FETCH_STATUS = 0
+	begin
+		update testowa..oddzialy
+		set NAZWA_ODD = 'nowa nazwa XD'
+		where current of kursor
+		fetch next from kursor into @id
+	end
+else
+	begin 
+		insert into testowa..oddzialy (NR_ODD ,NAZWA_ODD) values (3, 'oddzial trzeci inny')
+	end
+end
+close kursor
+deallocate kursor
+
+select * from testowa..oddzialy
+go
